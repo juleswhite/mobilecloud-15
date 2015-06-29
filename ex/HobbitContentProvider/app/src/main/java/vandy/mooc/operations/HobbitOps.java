e159e0077282946ec6ed7be8ec00ca43b95f58c5
Genius <ekpe6000@gmail.com>
2015-06-28 18:24:08 -0700

    fixed menu issues and cleaned up some code

---
+++ ex/HobbitContentProvider/app/src/main/java/vandy/mooc/operations/HobbitOps.java
@@ -0,0 +1,188 @@
+package vandy.mooc.operations;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.widget.SimpleCursorAdapter;
+
+import vandy.mooc.common.ConfigurableOps;
+
+/**
+ * Class that defines operations for inserting, querying, updating,
+ * and deleting characters from the HobbitContentProvider.  This class
+ * plays the role of the "Abstraction" in the Bridge pattern.  It
+ * implements ConfigurableOps so it can be managed by the
+ * GenericActivity framework.  This class and the hierarchy it
+ * abstracts play the role of the "Presenter" in the
+ * Model-View-Presenter pattern.
+ */
+public class HobbitOps implements ConfigurableOps {
+    /**
+     * Means for accessing the ContentProvider (i.e., CONTENT_RESOLVER
+     * or CONTENT_PROVIDER_CLIENT) for the HobbitOps implementation.
+     */
+    public enum ContentProviderAccessMeans {
+        CONTENT_RESOLVER,
+        CONTENT_PROVIDER_CLIENT
+    }
+
+    /**
+     * innitialize the access means to content resolver
+     */
+    private ContentProviderAccessMeans mAccessMeans =
+        ContentProviderAccessMeans.CONTENT_RESOLVER;
+
+    /**
+     * Reference to the designed Concrete Implementor (i.e., either
+     * HobbitOpsContentResolver or HobbitOpsContentProviderClient).
+     */
+    private HobbitOpsImpl mHobbitOpsImpl;
+
+    /**
+     * This default constructor must be public for the GenericOps
+     * class to work properly.
+     */
+    public HobbitOps() {
+        accessMeansImplTrigger();//pick the default implementation of the implementation for the access means
+
+    }
+
+    private void accessMeansImplTrigger() {
+        // Select the appropriate means of accessing the Content
+        // Provider.
+        switch(mAccessMeans) {
+        case CONTENT_RESOLVER:
+            mHobbitOpsImpl =
+                new HobbitOpsContentResolver();//at the first time, the default will be the content resolver
+            break;
+        case CONTENT_PROVIDER_CLIENT:
+            mHobbitOpsImpl =
+                new HobbitOpsContentProviderClient();
+            break;
+        }
+     }
+
+    /**
+     * Hook method dispatched by the GenericActivity framework to
+     * initialize the HobbitOps object after it's been created.
+     *
+     * @param activity     The currently active Activity.  
+     * @param firstTimeIn  Set to "true" if this is the first time the
+     *                     Ops class is initialized, else set to
+     *                     "false" if called after a runtime
+     *                     configuration change.
+     */
+    @Override
+    public void onConfiguration(Activity activity,
+                                boolean firstTimeIn) {
+        mHobbitOpsImpl.onConfiguration(activity,
+                                       firstTimeIn);
+    }
+    
+    /**
+     * Release resources to prevent leaks.
+     */
+    public void close() {
+        mHobbitOpsImpl.close();
+    }
+
+    /**
+     * Return a @a SimpleCursorAdapter that can be used to display the
+     * contents of the Hobbit ContentProvider.
+     * it uses factory method pattern under the hood
+     */
+    public SimpleCursorAdapter makeCursorAdapter() {
+        return mHobbitOpsImpl.makeCursorAdapter();
+    }
+
+    /**
+     * Insert a Hobbit @a character of a particular @a race into the
+     * HobbitContentProvider.
+     */
+    public Uri insert(String character,
+                      String race) throws RemoteException {
+        return mHobbitOpsImpl.insert(character,
+                                     race);
+    }
+
+    /**
+     * Insert an array of Hobbit @a characters of a particular @a race
+     * into the HobbitContentProvider.
+     */
+    public int bulkInsert(String[] characters,
+                          String race) throws RemoteException {
+        return mHobbitOpsImpl.bulkInsert(characters,
+                                         race);
+    }
+
+    /**
+     * Update the @a name and @a race of a Hobbit character at a designated 
+     * @a uri from the HobbitContentProvider.
+     */
+    public int updateByUri(Uri uri,
+                           String name,
+                           String race) throws RemoteException {
+        return mHobbitOpsImpl.updateByUri(uri,
+                                          name,
+                                          race);
+    }
+
+    /**
+     * Update the @a race of a Hobbit character with the given @a
+     * name.
+     */
+    public int updateRaceByName(String name,
+                                String race) throws RemoteException {
+        return mHobbitOpsImpl.updateRaceByName(name,
+                                               race);
+    }
+
+    /**
+     * Delete an array of Hobbit @a characterNames from the
+     * HobbitContentProvider.
+     */
+    public int deleteByName(String[] characterNames) 
+        throws RemoteException {
+        return mHobbitOpsImpl.deleteByName(characterNames);
+    }
+
+    /**
+     * Delete an array of Hobbit @a characterRaces from the
+     * HobbitContentProvider.
+     */
+    public int deleteByRace(String[] characterRaces)
+        throws RemoteException {
+        return mHobbitOpsImpl.deleteByRace(characterRaces);
+    }
+
+    /**
+     * Delete all characters in the HobbitContentProvider.
+     */
+    public int deleteAll()
+        throws RemoteException {
+        return mHobbitOpsImpl.deleteAll();
+    }
+
+    /**
+     * Display all the current contents of the HobbitContentProvider.
+     */
+    public void displayAll()
+        throws RemoteException {
+        mHobbitOpsImpl.displayAll();
+    }
+
+    /**
+     * Sets the means for accessing the ContentProvider (i.e.,
+     * CONTENT_RESOLVER or CONTENT_PROVIDER_CLIENT) for the HobbitOps
+     * implementation.
+     */
+    public void setContentProviderAccessMeans(ContentProviderAccessMeans accessMeans) {
+        if(this.mAccessMeans!=accessMeans){
+            mAccessMeans = accessMeans;
+            accessMeansImplTrigger();//trigger to change the implementation fo the content provider based on the access Means
+
+        }
+        System.out.println("name of hobit ops impl"+mHobbitOpsImpl.getClass().getSimpleName());
+
+    }
+}
